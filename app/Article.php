2077b478e7d834c71a23088b8d917c5e6ed61876
minisera <koki.ohtaka0821@gmail.com>
2021-02-10 13:10:11 +0900

    feat: 記事更新削除機能追加のため、コントローラー、ビュー、ルーティング変更

--- app/Article.php
+++ app/Article.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
-{
+{   
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
     public function user():BelongsTo
     {
         return $this->belongsTo('App\User');
--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
@@ -17,5 +18,35 @@ class ArticleController extends Controller
     public function create()
     {
         return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request,Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit',compact('article'));
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show',compact('article'));
     }
 }
